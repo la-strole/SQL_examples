cfb4ef03ff7a00c19a855e65d2ab430eff1f1aaa
la-strole <sovaleto48@gmail.com>
2021-04-10 01:01:26 +0200

    add file download and pandas to import csv to sql

--- covid_russia.py
+++ covid_russia.py
@@ -1,38 +1,52 @@
 import sqlite3
 import sys
 import matplotlib.pyplot as plt
+import requests
+import pandas
 
 
 def main():
-    # open sqlite database from cli arg
-    if len(sys.argv) != 2:
-        print("usage: covid_russia.py <name_of_sqlite_database.db>")
-        return 1
+    # download fresh csv file
+    url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+    csv_name = "covid_19.csv"
+    with open(csv_name, "w") as local_file:
+        local_file.write(requests.get(url).text)
 
-    con = sqlite3.connect(f"{sys.argv[1]}")
+    # copy csv to database
+    db_name = "covid_19.db"
+    table_names = ['covid_19']
+
+    file_db = open(db_name, "w").close()
+    con = sqlite3.connect(db_name)
+
+    pandas.read_csv(csv_name).to_sql(table_names[0], con, if_exists='append', index=False)
 
     date = []
     cases = []
     deaths = []
 
-    for row in con.execute('SELECT date, total_cases, total_deaths '
-                           'FROM covid_table WHERE location LIKE "Russia";'):
+    location = 'Russia'
+
+    for row in con.execute('SELECT date, new_cases, total_deaths '
+                           'FROM {} WHERE location LIKE ?;'.format(table_names[0]), [location]):
         date.append(row[0])
-        case = row[1].rstrip('.0')
-        death = row[2].rstrip('.0')
-        if case != '':
-            cases.append(int(row[1].rstrip('.0')))
+        case = row[1]
+        death = row[2]
+        if case:
+            cases.append(int(row[1]))
         else:
             cases.append(0)
-        if death != '':
-            deaths.append(int(row[2].rstrip('.0')))
+        if death:
+            deaths.append(int(row[2]))
         else:
             deaths.append(0)
+
+    con.close()
 
     count = list(range(len(cases)))
     fig, ax = plt.subplots()
     ax.plot(count, cases, linestyle='solid', label='cases')
-    ax.set(xlabel='date', ylabel="cases", title="covid in Russia")
+    ax.set(xlabel='date', ylabel="cases", title=f"covid in {location}")
     ax.plot(count, deaths, linestyle='dashed', label='deaths')
 
     ax.legend()
