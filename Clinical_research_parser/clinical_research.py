609d864a0f4365788f8a74f5a401f5df319f535c
la-strole <la-strole.sovaleto48@gmail.com>
2021-12-25 15:10:33 +0300

    Add clinical_research

--- Clinical_research_parser/clinical_research.py
+++ Clinical_research_parser/clinical_research.py
@@ -6,13 +6,18 @@ import sqlite3
 import os
 
 
-def main():
-    # download the csv file
+def main(serach_tag:str):
 
+    """
+    Looking for clinical trials in world clinical trials base (https://clinicaltrials.gov)
+    Look for tag name, status = recruting.
+    Return list of links to trials.
+    """
     csv_filename = 'clinical_trials.csv'
 
     path = "https://clinicaltrials.gov/ct2/results/download_fields?cntry=RU&down_count=10000&down_fmt=csv&down_flds=all"
     payload = {'cntry': 'RU', 'down_count': '10000', 'down_fmt': 'csv', 'down_flds': 'all'}
+    
     r = requests.get(path, params=payload)
     if r.status_code != 200:
         print(f'can not download data. status code = {r.status_code}\n', sys.stderr)
@@ -21,18 +26,19 @@ def main():
     with open(csv_filename, 'w') as csv_file:
         csv_file.write(r.text)
 
-    # create database in memory
+    # Create database in memory
     con = sqlite3.connect(':memory:')
     cur = con.cursor()
 
-    # copy the data from the csv file to sqlite db in memory with pandas lib
+    # Copy the data from the csv file to sqlite db in memory with pandas lib
     tables = ['clinical_res']
     pandas.read_csv(csv_filename).to_sql(tables[0], con, if_exists='append', index=False)
 
     cur.execute(f'SELECT COUNT(rowid) FROM {tables[0]}')
     print(f'total number of lines in DB: {cur.fetchone()[0]}\n')
-    # make search in DB
-    data = collections.OrderedDict([('Title', '%BRAF%'), ('Status', 'Recruiting')])
+    
+    # Make search in DB
+    data = collections.OrderedDict([('Title', f'%{search_tag}%'), ('Status', 'Recruiting')])
 
     cur.execute(f'SELECT Title, url FROM {tables[0]} WHERE {list(data.keys())[0]} LIKE ? AND '
                 f'{list(data.keys())[1]} = ?;', [f'%{data["Title"]}%', data['Status']])
@@ -42,7 +48,7 @@ def main():
 
     con.close()
 
-    while 1:
+    while True:
         user_inout = input('Do you want to delete csv file from your PC (y/n)')
         if user_inout == 'y' or 'Y':
             try:
@@ -58,4 +64,5 @@ def main():
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    search_tag = input("input search tag: ")
+    sys.exit(main(search_tag))
